7a01658db48b1106c0ac9aec59d56c7204826f22
amadou <diabyamadou15@gmail.com>
2022-01-12 10:51:08 +0100

    Sauv

---
+++ borderify.js
@@ -0,0 +1,4 @@
+alert('Bonjours sur mon site pour des raisons de sécurité le texte du site sera en rouge !');
+document.body.style.border = "5px solid red";
+document.body.style.color ="red";
+document.body.style.textTransform ="lowercase" ;
